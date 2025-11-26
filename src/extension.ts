019f926f4fef42dde17c319d1e8b933d84ebb777
rainbowatcher <rainbow-w@qq.com>
2025-11-26 11:42:47 +0800

    refactor(extension): extract SrccEditorProvider registration to variable for cleaner code organization in activate function
    Remove commented openDocumentDisposable and simplify subscriptions push to improve readability and maintainability.

--- src/extension.ts
+++ src/extension.ts
@@ -5,18 +5,14 @@ import { toggleAutoOpen } from "./commands/toggleAutoOpen"
 export function activate(context: vscode.ExtensionContext) {
   console.log("SRCC extension is now active!")
 
-  // register toggle auto open command
+  const srccEditorProvider = SrccEditorProvider.register(context)
+  // command
   const toggleAutoOpenCommand = vscode.commands.registerCommand(
     "srcc.toggleAutoOpen",
     toggleAutoOpen,
   )
 
-  // register functions
-  context.subscriptions.push(
-    // openDocumentDisposable,
-    toggleAutoOpenCommand,
-    SrccEditorProvider.register(context),
-  )
+  context.subscriptions.push(toggleAutoOpenCommand, srccEditorProvider)
 }
 
 export function deactivate() {
